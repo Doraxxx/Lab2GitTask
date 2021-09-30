5a9f73b983ac05c7eb0253d44d8c056ec74760ff
Doraxxx <ryumina.darya28@gmail.com>
2021-09-30 16:25:45 +0300

    A simple calculator

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char again = 'д';
+            while (again == 'д')
+            {
+                double a;
+                double b;
+                double total;
+                char oper;
+
+                Console.WriteLine("Введите первое число:");
+                a = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Введите оператор:");
+                oper = Convert.ToChar(Console.ReadLine());
+
+                Console.WriteLine("Введите второе число:");
+                b = Convert.ToDouble(Console.ReadLine());
+
+                if (oper == '+')
+                {
+                    total = a + b;
+                    Console.WriteLine(a + " + " + b + " = " + total);
+                }
+
+                else if (oper == '-')
+                {
+                    total = a - b;
+                    Console.WriteLine(a + " - " + b + " = " + total);
+                }
+
+                else if (oper == '*')
+                {
+                    total = a * b;
+                    Console.WriteLine(a + " - " + b + " = " + total);
+                }
+
+                else if (oper == '/')
+                {
+                    total = a / b;
+                    Console.WriteLine(a + " / " + b + " = " + total);
+                }
+                else
+                {
+                    Console.WriteLine("Неизвестный оператор.");
+                }
+                Console.WriteLine("Вы хотите продолжить работу с калькулятором? (д/н)");
+                again = Convert.ToChar(Console.ReadLine());
+            }
+        }
+    }
+}
