b8802492d4ea86863560ac0a7a7f9f11e7299f14
Doraxxx <ryumina.darya28@gmail.com>
2021-09-30 17:31:55 +0300

    addCosSin commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,55 +9,93 @@ namespace ConsoleApp1
             char again = 'д';
             while (again == 'д')
             {
-                double a;
-                double b;
-                double total;
-                char oper;
+                char typeOper;
 
-                Console.WriteLine("Введите первое число:");
-                a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Выберите тип операции: с - стандартные (+, -, *, /), д - дополнительные (sin, cos)");
+                typeOper = Convert.ToChar(Console.ReadLine());
 
-                Console.WriteLine("Введите оператор:");
-                oper = Convert.ToChar(Console.ReadLine());
-
-                Console.WriteLine("Введите второе число:");
-                b = Convert.ToDouble(Console.ReadLine());
-
-                if (oper == '+')
+                switch (typeOper)
                 {
-                    total = a + b;
-                    Console.WriteLine(a + " + " + b + " = " + total);
+                    case 'с':
+
+                        double a;
+                        double b;
+                        double total;
+                        char oper;
+
+                        Console.WriteLine("Введите первое число:");
+                        a = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Введите оператор:");
+                        oper = Convert.ToChar(Console.ReadLine());
+
+                        Console.WriteLine("Введите второе число:");
+                        b = Convert.ToDouble(Console.ReadLine());
+
+                        if (oper == '+')
+                        {
+                            total = a + b;
+                            Console.WriteLine(a + " + " + b + " = " + total);
+                        }
+
+                        else if (oper == '-')
+                        {
+                            total = a - b;
+                            Console.WriteLine(a + " - " + b + " = " + total);
+                        }
+
+                        else if (oper == '*')
+                        {
+                            total = a * b;
+                            Console.WriteLine(a + " * " + b + " = " + total);
+                        }
+
+                        else if (oper == '/')
+                        {
+                            if (b == 0)
+                            {
+                                Console.WriteLine("Деление на ноль невозможно");
+                            }
+                            else
+                            {
+                                total = a / b;
+                                Console.WriteLine(a + " / " + b + " = " + total);
+                            }
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Неизвестный оператор.");
+                        }
+
+                        break;
+
+                    case 'д':
+
+                        double ug;
+                        double znach;
+                        char mathOp;
+
+                        Console.WriteLine("Введите угол:");
+                        ug = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Введите математическую функцию (s для вычисления sin, c для вычисления cos):");
+                        mathOp = Convert.ToChar(Console.ReadLine());
+
+                        if (mathOp == 's')
+                        {
+                            Console.WriteLine("sin " + ug + " = " + Math.Sin(ug));
+                        }
+
+                        else if (mathOp == 'c')
+                        {
+                            Console.WriteLine("cos " + ug + " = " + Math.Cos(ug));
+                        }
+
+                        break;
                 }
 
-                else if (oper == '-')
-                {
-                    total = a - b;
-                    Console.WriteLine(a + " - " + b + " = " + total);
-                }
-
-                else if (oper == '*')
-                {
-                    total = a * b;
-                    Console.WriteLine(a + " - " + b + " = " + total);
-                }
-
-                else if (oper == '/')
-                {
-                    if (b == 0)
-                    {
-                        Console.WriteLine("Деление на ноль невозможно");
-                    }
-                    else
-                    {
-                        total = a / b;
-                        Console.WriteLine(a + " / " + b + " = " + total);
-                    }
-                    
-                }
-                else
-                {
-                    Console.WriteLine("Неизвестный оператор.");
-                }
+                
                 Console.WriteLine("Вы хотите продолжить работу с калькулятором? (д/н)");
                 again = Convert.ToChar(Console.ReadLine());
             }
